dd6e08b9e1afb65d0fe1025eb591ff39e2f00e54
msvdev-k <msvdev.k@gmail.com>
2023-09-16 21:12:07 +0300

    исправлены ошибки в sql файле

--- src/main/resources/db/migration/V1__Create_file_table.sql
+++ src/main/resources/db/migration/V1__Create_file_table.sql
@@ -3,7 +3,7 @@
 -- -------------------------------------------------- --
 
 CREATE TABLE "categories" (
-    "id"          SERIAL,
+    "id"          SERIAL       PRIMARY KEY,
     "name"        VARCHAR(64)  NOT NULL,
     "description" VARCHAR(256) NULL,
 
@@ -12,12 +12,12 @@ CREATE TABLE "categories" (
 
 
 CREATE TABLE "expenses" (
-    "id"          SERIAL,
+    "id"          SERIAL        PRIMARY KEY,
     "date"        DATE          NOT NULL,
     "category_id" INTEGER       NOT NULL,
     "cost"        NUMERIC(12,2) NOT NULL,
     "state"       VARCHAR(32)   NULL,
 
     CONSTRAINT "positive_cost" CHECK ("cost" >= 0),
-    CONSTRAINT "fk_category"   FOREIGN KEY "category_id" REFERENCES "categories" ("id")
+    CONSTRAINT "fk_category"   FOREIGN KEY ("category_id") REFERENCES "categories" ("id")
 );
